ce4a56e18bdaf5cec187abd87aab0e6b616f9c14
Niko Heikkilä <niko.heikkila@futurice.com>
2022-01-22 14:37:29 +0200

    fix: correct test name

--- tests/test_git.py
+++ tests/test_git.py
@@ -4,5 +4,5 @@ from publicator import git
 def test_get_current_branch():
     assert git.current_branch() == "main"
 
-def test_get_release_brances():
+def test_get_release_branches():
     assert git.release_branches() == ("main", "master")
