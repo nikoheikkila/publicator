b8379d6568fe9cbadcdd50ef6e3f35cf59f06994
Niko Heikkilä <niko.heikkila@futurice.com>
2022-01-22 20:41:09 +0200

    feat: handle shell command errors

--- publicator/shell.py
+++ publicator/shell.py
@@ -1,5 +1,20 @@
 import subprocess
 from shlex import split
 
+class ShellException(OSError):
+    pass
+
 def run(command: str) -> list[str]:
-    return subprocess.run(split(command), stdout=subprocess.PIPE).stdout.decode('utf-8').splitlines()
+    result = subprocess.run(split(command), stdout=subprocess.PIPE)
+    stdout = format(result.stdout)
+
+    if not ok(result):
+        raise ShellException(stdout)
+
+    return stdout
+
+def format(stream: bytes) -> list[str]:
+    return stream.decode('utf-8').splitlines()
+
+def ok(result: subprocess.CompletedProcess) -> bool:
+    return result.returncode == 0
