295c97a17213399cb853f8d316591c312bcf37cd
Niko Heikkilä <niko.heikkila@futurice.com>
2022-01-23 18:46:49 +0200

    fix: assert correct error message on Windows

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -1,6 +1,10 @@
 from subprocess import CalledProcessError
+import sys
 from pytest import raises
 from publicator import shell
+
+def is_windows() -> bool:
+    return sys.platform.startswith("win")
 
 def test_run_with_empty_command():
     result = shell.run("")
@@ -15,7 +19,9 @@ def test_run_command_with_multiline_output():
     assert result == ["Hello", "World"]
 
 def test_run_with_nonexistent_command():
-    with raises(FileNotFoundError, match="No such file or directory"):
+    pattern = "The system cannot find the file specified" if is_windows() else "No such file or directory"
+
+    with raises(FileNotFoundError, match=pattern):
         shell.run("123")
 
 def test_run_with_failing_command():
