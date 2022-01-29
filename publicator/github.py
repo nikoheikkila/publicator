216c403016440c7bb185ce672420daed20d3b0b1
Niko Heikkilä <niko.heikkila@futurice.com>
2022-01-29 13:33:08 +0200

    refactor: pass object instead of primitives

--- publicator/github.py
+++ publicator/github.py
@@ -1,11 +1,10 @@
 from urllib.parse import urlencode
+from publicator.git import Repo
 from publicator.semver import Semver
 
 
-def new_release_url(
-    owner: str, repository: str, tag: Semver, title: str = "", body: str = "", pre_release: bool = False
-) -> str:
-    base_url = f"https://github.com/{owner}/{repository}/releases/new?"
+def new_release_url(repo: Repo, tag: Semver, title: str = "", body: str = "", pre_release: bool = False) -> str:
+    base_url = f"https://{repo.server}/{repo.owner}/{repo.name}/releases/new?"
     query = {"tag": tag, "title": title, "body": body, "prerelease": int(pre_release)}
 
     return base_url + urlencode(query)
--- tests/test_github.py
+++ tests/test_github.py
@@ -1,20 +1,31 @@
+from pytest import fixture
 from publicator import github
 from publicator.semver import Semver
+from publicator.git import Repo
 
 
-def test_new_release_url_with_required_variables() -> None:
-    url = github.new_release_url(owner="nikoheikkila", repository="publicator", tag=Semver(1, 2, 3))
+@fixture()
+def repo() -> Repo:
+    return Repo(server="github.com", owner="nikoheikkila", name="publicator")
+
+
+@fixture()
+def tag() -> Semver:
+    return Semver(1, 2, 3)
+
+
+def test_new_release_url_with_required_variables(repo: Repo, tag: Semver) -> None:
+    url = github.new_release_url(repo, tag)
     expected = "https://github.com/nikoheikkila/publicator/releases/new?tag=1.2.3&title=&body=&prerelease=0"
 
     assert url == expected
 
 
-def test_new_release_url_with_all_variables() -> None:
+def test_new_release_url_with_all_variables(repo: Repo, tag: Semver) -> None:
     url = github.new_release_url(
-        owner="nikoheikkila",
-        repository="publicator",
-        tag=Semver(1, 2, 3),
-        title="Version 1.2.3",
+        repo,
+        tag,
+        title=f"Version {tag}",
         body="# Changelog",
         pre_release=True,
     )
