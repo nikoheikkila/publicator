03d6b4a9dd878e7e10ebac6046bf5180167e6bea
Niko Heikkilä <niko.heikkila@futurice.com>
2022-10-12 20:15:14 +0300

    refactor(tests): group config tests into class

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,43 +4,40 @@ from unittest.mock import MagicMock
 from publicator.config import Configuration, NullConfiguration, factory
 
 
-def test_factory_resolves_to_real_configuration(mock_read_text: MagicMock) -> None:
-    mock_read_text.return_value = "[tool.publicator]"
+class TestConfig:
+    def test_factory_resolves_to_real_configuration(self, mock_read_text: MagicMock) -> None:
+        mock_read_text.return_value = "[tool.publicator]"
 
-    configuration = factory("pyproject.toml")
+        configuration = factory("pyproject.toml")
 
-    assert isinstance(configuration, Configuration)
+        assert isinstance(configuration, Configuration)
 
+    def test_factory_resolves_to_null_configuration_on_missing_file(self, tmp_path: Path) -> None:
+        filename = tmp_path / "missing.toml"
 
-def test_factory_resolves_to_null_configuration_on_missing_file(tmp_path: Path) -> None:
-    filename = tmp_path / "missing.toml"
+        configuration = factory(filename.as_posix())
 
-    configuration = factory(filename.as_posix())
+        assert isinstance(configuration, NullConfiguration)
 
-    assert isinstance(configuration, NullConfiguration)
+    def test_factory_resolves_to_null_configuration_on_missing_section(self, mock_read_text: MagicMock) -> None:
+        mock_read_text.return_value = "[tool.poetry]"
 
+        configuration = factory("pyproject.toml")
 
-def test_factory_resolves_to_null_configuration_on_missing_section(mock_read_text: MagicMock) -> None:
-    mock_read_text.return_value = "[tool.poetry]"
+        assert isinstance(configuration, NullConfiguration)
 
-    configuration = factory("pyproject.toml")
+    def test_get_from_configuration(self, mock_read_text: MagicMock) -> None:
+        mock_read_text.return_value = """
+        [tool.publicator]
+        repository = "pypi"
+        """
 
-    assert isinstance(configuration, NullConfiguration)
+        configuration = Configuration.from_path(Path("config.toml"))
 
+        assert configuration.get("repository") == "pypi"
 
-def test_get_from_configuration(mock_read_text: MagicMock) -> None:
-    mock_read_text.return_value = """
-    [tool.publicator]
-    repository = "pypi"
-    """
+    def test_get_from_null_configuration(self) -> None:
+        configuration = NullConfiguration.from_path(Path("config.toml"))
 
-    configuration = Configuration.from_path(Path("config.toml"))
-
-    assert configuration.get("repository") == "pypi"
-
-
-def test_get_from_null_configuration() -> None:
-    configuration = NullConfiguration.from_path(Path("config.toml"))
-
-    assert configuration.get("key") is None
-    assert configuration.get("key", "value") == "value"
+        assert configuration.get("key") is None
+        assert configuration.get("key", "value") == "value"
