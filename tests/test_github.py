ae08ca768979511f1036be0bf8de915bd1e7269f
Niko Heikkilä <niko.heikkila@futurice.com>
2022-10-12 20:17:05 +0300

    refactor(tests): group github tests into class

--- tests/test_github.py
+++ tests/test_github.py
@@ -4,41 +4,38 @@ from pytest import fixture, raises
 from semmy import Semver
 
 
-@fixture()
-def repo() -> Repo:
-    return Repo(server="github.com", owner="nikoheikkila", name="publicator")
+class TestGitHub:
+    @fixture()
+    def repo(self) -> Repo:
+        return Repo(server="github.com", owner="nikoheikkila", name="publicator")
 
+    @fixture()
+    def tag(self) -> Semver:
+        return Semver(1, 2, 3)
 
-@fixture()
-def tag() -> Semver:
-    return Semver(1, 2, 3)
+    def test_new_release_url_with_required_variables(self, repo: Repo, tag: Semver) -> None:
+        url = github.new_release_url(repo, tag)
+        expected = "https://github.com/nikoheikkila/publicator/releases/new?tag=1.2.3&title=&body=&prerelease=0"
 
+        assert url == expected
 
-def test_new_release_url_with_required_variables(repo: Repo, tag: Semver) -> None:
-    url = github.new_release_url(repo, tag)
-    expected = "https://github.com/nikoheikkila/publicator/releases/new?tag=1.2.3&title=&body=&prerelease=0"
+    def test_new_release_url_with_all_variables(self, repo: Repo) -> None:
+        tag = Semver(0, 1, 0)
 
-    assert url == expected
+        url = github.new_release_url(repo, tag, title=f"Version {tag}", body="# Changelog")
 
-
-def test_new_release_url_with_all_variables(repo: Repo) -> None:
-    tag = Semver(0, 1, 0)
-
-    url = github.new_release_url(repo, tag, title=f"Version {tag}", body="# Changelog")
-
-    expected = (
-        "https://github.com/nikoheikkila/publicator/releases/new?"
-        "tag=0.1.0&title=Version+0.1.0&body=%23+Changelog&prerelease=1"
-    )
-
-    assert url == expected
-
-
-def test_new_release_url_throws_for_non_github_repos(tag: Semver) -> None:
-    repo = Repo(server="bitbucket.org", owner="user", name="stuff")
-
-    with raises(AssertionError, match="Current repository is not hosted on github.com"):
-        github.new_release_url(
-            repo,
-            tag,
+        expected = (
+            "https://github.com/nikoheikkila/publicator/releases/new?"
+            "tag=0.1.0&title=Version+0.1.0&body=%23+Changelog&prerelease=1"
         )
+
+        assert url == expected
+
+    def test_new_release_url_throws_for_non_github_repos(self, tag: Semver) -> None:
+        repo = Repo(server="bitbucket.org", owner="user", name="stuff")
+
+        with raises(AssertionError, match="Current repository is not hosted on github.com"):
+            github.new_release_url(
+                repo,
+                tag,
+            )
