7fc0f8b7404c5c3ca873cc43e5b29e61f79c4935
Niko Heikkilä <niko.heikkila@futurice.com>
2022-10-12 20:04:11 +0300

    refactor(tests): group shell tests into class

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -4,32 +4,29 @@ from pytest import raises
 from publicator import shell
 
 
-def is_windows() -> bool:
-    return sys.platform.startswith("win")
+class TestShell:
+    @staticmethod
+    def is_windows() -> bool:
+        return sys.platform.startswith("win")
 
+    def test_run_with_empty_command(self) -> None:
+        result = shell.run("")
+        assert not result
 
-def test_run_with_empty_command() -> None:
-    result = shell.run("")
-    assert not result
+    def test_run_with_single_command(self) -> None:
+        result = shell.run('echo "Hello World!"')
+        assert result.pop() == "Hello World!"
 
+    def test_run_command_with_multiline_output(self) -> None:
+        result = shell.run('echo "Hello\nWorld"')
+        assert result == ["Hello", "World"]
 
-def test_run_with_single_command() -> None:
-    result = shell.run('echo "Hello World!"')
-    assert result.pop() == "Hello World!"
+    def test_run_with_nonexistent_command(self) -> None:
+        pattern = "The system cannot find the file specified" if self.is_windows() else "No such file or directory"
 
+        with raises(FileNotFoundError, match=pattern):
+            shell.run("123")
 
-def test_run_command_with_multiline_output() -> None:
-    result = shell.run('echo "Hello\nWorld"')
-    assert result == ["Hello", "World"]
-
-
-def test_run_with_nonexistent_command() -> None:
-    pattern = "The system cannot find the file specified" if is_windows() else "No such file or directory"
-
-    with raises(FileNotFoundError, match=pattern):
-        shell.run("123")
-
-
-def test_run_with_failing_command() -> None:
-    with raises(CalledProcessError, match="returned non-zero exit status 1"):
-        shell.run("false")
+    def test_run_with_failing_command(self) -> None:
+        with raises(CalledProcessError, match="returned non-zero exit status 1"):
+            shell.run("false")
