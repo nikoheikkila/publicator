05ab2421a68e3ed4aae9f06cce65e2ac93b73e8e
Niko Heikkilä <niko.heikkila@futurice.com>
2022-10-12 20:06:14 +0300

    refactor(tests): group poetry tests into class

--- tests/test_poetry.py
+++ tests/test_poetry.py
@@ -3,41 +3,36 @@ from publicator import poetry
 from semmy import Semver
 
 
-def test_poetry_install(mock_shell: MagicMock) -> None:
-    mock_shell.return_value = [
-        "Installing dependencies from lock file",
-        "No dependencies to install or update",
-        "Installing the current project: publicator (1.2.3)",
-    ]
+class TestPoetry:
+    def test_poetry_install(self, mock_shell: MagicMock) -> None:
+        mock_shell.return_value = [
+            "Installing dependencies from lock file",
+            "No dependencies to install or update",
+            "Installing the current project: publicator (1.2.3)",
+        ]
 
-    assert poetry.install()
+        assert poetry.install()
 
+    def test_run_pytest(self, mock_shell: MagicMock) -> None:
+        mock_shell.return_value = ["test session starts"]
+        assert poetry.run("pytest")
 
-def test_run_pytest(mock_shell: MagicMock) -> None:
-    mock_shell.return_value = ["test session starts"]
-    assert poetry.run("pytest")
+    def test_run_unittest(self, mock_shell: MagicMock) -> None:
+        mock_shell.return_value = ["Ran 1 test in 0.000s"]
+        assert poetry.run("python -m unittest")
 
+    def test_verify_project_health(self, mock_shell: MagicMock) -> None:
+        mock_shell.return_value = ["All set!"]
+        assert poetry.ok()
 
-def test_run_unittest(mock_shell: MagicMock) -> None:
-    mock_shell.return_value = ["Ran 1 test in 0.000s"]
-    assert poetry.run("python -m unittest")
+    def test_building_package(self, mock_shell: MagicMock) -> None:
+        mock_shell.return_value = ["Building publicator (1.2.3)"]
+        assert poetry.build()
 
+    def test_publishing_the_package(self, mock_shell: MagicMock) -> None:
+        mock_shell.return_value = ["Publishing publicator (1.2.3)"]
+        assert poetry.publish(repository=None)
 
-def test_verify_project_health(mock_shell: MagicMock) -> None:
-    mock_shell.return_value = ["All set!"]
-    assert poetry.ok()
-
-
-def test_building_package(mock_shell: MagicMock) -> None:
-    mock_shell.return_value = ["Building publicator (1.2.3)"]
-    assert poetry.build()
-
-
-def test_publishing_the_package(mock_shell: MagicMock) -> None:
-    mock_shell.return_value = ["Publishing publicator (1.2.3)"]
-    assert poetry.publish(repository=None)
-
-
-def test_version_is_returned(mock_shell: MagicMock) -> None:
-    mock_shell.return_value = ["1.2.3"]
-    assert poetry.version() == Semver(1, 2, 3)
+    def test_version_is_returned(self, mock_shell: MagicMock) -> None:
+        mock_shell.return_value = ["1.2.3"]
+        assert poetry.version() == Semver(1, 2, 3)
